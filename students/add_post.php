a8f7aa6391969f1ae9d937d94f960a4cce63eaa5
Abdiel Arias <81927109+LORDPRN@users.noreply.github.com>
2021-06-09 14:30:25 -0500

    Update add_post.php

--- students/add_post.php
+++ students/add_post.php
@@ -13,6 +13,15 @@ $conn = mysqli_connect($sname, $unmae, $password2, $db_name);
 $descripcionC= $_POST['descripcionC'];
 $archivoC= $_POST['archivoC'];
 
+if($_FILES['archivoC']){
+	$archivoC = md5($_FILES['archivoC']. time()) . "." . end(explode('.', $_FILES['archivoC']));
+	$NewFile = "../images/" . $archivoC;
+	if (!move_uploaded_file($_FILES['archivoC'], $NewFile))
+		{
+		die("Failed to move file " .$_FILES['archivoC']." to " . $archivoC);	
+		}	
+		
+}
 //Meter la data mediante una sentencia sql para agregar un usuario a la base de datos
 $sql = "INSERT INTO contenidos (descripcionC,archivoC) VALUES ('$descripcionC','$archivoC')";
 
@@ -20,7 +29,8 @@ if (mysqli_query($conn, $sql)) {
   
 	header("Location: home_pro.php?notify=Se ha hecho tu publicación");
 	exit();
-}
+} 
+
 
 
 
